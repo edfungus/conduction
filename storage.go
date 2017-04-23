eb46974b0eaa633a75ec9abc3ae32d66c5be1460
edfungus <edfungus@gmail.com>
2017-04-23 13:45:32 -0700

    Cayley read/write flow

--- storage.go
+++ storage.go
@@ -7,16 +7,28 @@ import (
 	"github.com/cayleygraph/cayley"
 	"github.com/cayleygraph/cayley/graph"
 	_ "github.com/cayleygraph/cayley/graph/sql"
+	"github.com/cayleygraph/cayley/quad"
+	"github.com/cayleygraph/cayley/schema"
 	"github.com/edfungus/conduction/pb"
+	"github.com/pborman/uuid"
 )
 
 type Storage interface {
-	AddFlow(flow pb.Flow) (int64, error)
-	SavePath(path pb.Path) error
+	AddFlow(flow *pb.Flow) (uuid.UUID, error)
+	ReadFlow(uuid uuid.UUID) (*pb.Flow, error)
+	SavePath(path *pb.Path) error
 }
 
 type GraphStorage struct {
 	store *cayley.Handle
+	qw    graph.BatchWriter
+}
+
+type flowDTO struct {
+	rdfType     struct{} `quad:"@type > flowDTO"`
+	ID          string   `quad:"@id"`
+	Name        string   `quad:"name"`
+	Description string   `quad:"description,opt"`
 }
 
 const (
@@ -35,16 +47,50 @@ type GraphStorageConfig struct {
 func NewGraphStorage(config *GraphStorageConfig) (*GraphStorage, error) {
 	databasePath := fmt.Sprintf(DATABASE_URL, config.User, config.Host, config.Port, config.DatabaseName)
 	initDatabase(databasePath, config.DatabaseName)
-
 	opts := graph.Options{"flavor": config.DatabaseType}
-	err := graph.InitQuadStore("sql", databasePath, opts)
+	graph.InitQuadStore("sql", databasePath, opts)
 	store, err := cayley.NewGraph("sql", databasePath, opts)
 	if err != nil {
 		return nil, err
 	}
 	return &GraphStorage{
 		store: store,
+		qw:    graph.NewWriter(store),
 	}, nil
+}
+
+func (gs *GraphStorage) AddFlow(flow *pb.Flow) (string, error) {
+	newUUID := uuid.NewRandom().String()
+	fmt.Println(newUUID)
+	test := flowDTO{
+		ID:          newUUID,
+		Name:        flow.Name,
+		Description: flow.Description,
+	}
+	_, err := schema.WriteAsQuads(gs.qw, test)
+	if err != nil {
+		return "", err
+	}
+	return newUUID, nil
+}
+
+func (gs *GraphStorage) ReadFlow(uuid string) (*pb.Flow, error) {
+	var flowDTO flowDTO
+	err := schema.LoadTo(nil, gs.store, &flowDTO, toQuadID(uuid))
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Flow{
+		Name:        flowDTO.Name,
+		Description: flowDTO.Description,
+	}, nil
+}
+
+// func (gs *GraphStorage) SavePath(path pb.Path) error {
+// }
+
+func toQuadID(uuid string) quad.IRI {
+	return quad.IRI(uuid).Full().Short()
 }
 
 func initDatabase(connectionPath string, databaseName string) error {
--- storage_integration_test.go
+++ storage_integration_test.go
@@ -4,6 +4,9 @@ package main_test
 
 import (
 	. "github.com/edfungus/conduction"
+	"github.com/edfungus/conduction/pb"
+
+	"fmt"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -14,7 +17,7 @@ var _ = Describe("Conduction", func() {
 		var (
 			graph  *GraphStorage
 			config = &GraphStorageConfig{
-				// Host:         databaseHost, // To be set in test
+				Host:         databaseHost,
 				Port:         databasePort,
 				User:         "root",
 				DatabaseName: databaseName,
@@ -22,7 +25,6 @@ var _ = Describe("Conduction", func() {
 			}
 		)
 		BeforeEach(func() {
-			config.Host = databaseHost
 			var err error
 			graph, err = NewGraphStorage(config)
 			Expect(err).To(BeNil())
@@ -31,19 +33,53 @@ var _ = Describe("Conduction", func() {
 		Describe("Given creating a new Storage", func() {
 			Context("When Cockroach path is wrong or not available", func() {
 				It("Then should return an error", func() {
-					config.Host = "badHost"
-					_, err := NewGraphStorage(config)
+					badConfig := &GraphStorageConfig{
+						Host:         config.Host,
+						Port:         8888,
+						User:         config.User,
+						DatabaseName: config.DatabaseName,
+						DatabaseType: config.DatabaseType,
+					}
+					_, err := NewGraphStorage(badConfig)
 					Expect(err).ToNot(BeNil())
 				})
 			})
 			Context("When Cockroach path is correct and available", func() {
 				It("Then GraphStorage should return without error", func() {
-					config.Host = databaseHost
 					graph, err := NewGraphStorage(config)
 					Expect(err).To(BeNil())
 					Expect(graph).ToNot(BeNil())
 				})
 			})
+		})
+		Describe("Given saving a Flow to graph", func() {
+			Context("When the Flow is simplified to name and description", func() {
+				It("Then the Flow should be inserted and an UUID is returned", func() {
+					flow := &pb.Flow{
+						Name: "Edmund",
+					}
+					id, err := graph.AddFlow(flow)
+					Expect(err).To(BeNil())
+					Expect(id).ToNot(BeNil())
+
+					fmt.Println(id)
+
+					newFlow, err := graph.ReadFlow(id)
+					Expect(err).To(BeNil())
+					Expect(newFlow.Name).To(Equal(flow.Name))
+					Expect(newFlow.Description).To(Equal(flow.Description))
+				})
+			})
+			// Context("When the Path exists", func() {
+			// 	It("Then the Flow should be inserted connected to existing Path", func() {
+
+			// 	})
+			// })
+			// Context("When the Path does not exist", func() {
+			// 	It("Then the Path and Flow should also be inserted connected to Path", func() {
+
+			// 	})
+			// })
 		})
 	})
 })
